e5b4c765666aaacb2bd32c5f9f48f26970eeb5bc
Ashwin <ashwin.dinesh01@gmail.com>
2018-07-07 19:24:53 +0530

    Added static functions

--- Example/IOSStringUtils/ViewController.swift
+++ Example/IOSStringUtils/ViewController.swift
@@ -3,11 +3,15 @@ import UIKit
 import IOSStringUtils
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        print(StringUtils.isBlank(input: ""))
+        print(StringUtils.isEmpty(input: ""))
+        print(StringUtils.getLength(input: "abc"))
+        print(StringUtils.getCount(input: "abcaxyza", char: "a"))
+        print(StringUtils.getTitleCase(input: "game of  thrones"))
     }
 
 }
--- IOSStringUtils/Classes/StringUtils.swift
+++ IOSStringUtils/Classes/StringUtils.swift
@@ -2,22 +2,22 @@
 import Foundation
 public class StringUtils {
     
-    public func isBlank(input: String?) -> Bool {
+    static public func isBlank(input: String?) -> Bool {
         return input == nil || input!.trimmingCharacters(in: .whitespaces).count == 0
     }
     
-    public func isEmpty(input: String?) -> Bool {
+    static public func isEmpty(input: String?) -> Bool {
         return input == nil || input!.count == 0
     }
     
-    public func getLength(input: String?) -> Int {
+    static public func getLength(input: String?) -> Int {
         if isEmpty(input: input) {
             return 0
         }
         return input!.count
     }
     
-    public func getCount(input: String?, char: Character?) -> Int {
+    static public func getCount(input: String?, char: Character?) -> Int {
         if isEmpty(input: input) || char == nil {
             return 0
         }
@@ -30,32 +30,11 @@ public class StringUtils {
         return count
     }
     
-    public func getTitleCase(input: String) -> String {
+    static public func getTitleCase(input: String) -> String {
         if isBlank(input: input) {
             return input
         }
-        
         return input.capitalized
-        
-        /*var titleCase: String = ""
-        let arr = input.split(separator: " ")
-        
-        var shouldTitleize: Bool = true
-        for char in input {
-            var ch = char
-            if ch == " " {
-                shouldTitleize = true
-            } else if shouldTitleize {
-                ch = String(char).capitalized
-                shouldTitleize = false
-            } else {
-                
-            }
-            
-            titleCase.append(ch)
-        }
-        
-        return titleCase*/
     }
     
 }
